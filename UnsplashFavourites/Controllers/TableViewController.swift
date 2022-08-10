dc6aaa38e3e511a1a2b0af887cd599d5e0d446b6
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-08-10 19:10:32 +0300

    start setting table view

--- UnsplashFavourites/Controllers/TableViewController.swift
+++ UnsplashFavourites/Controllers/TableViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    let detailVC  = DetailViewController()
+    var favouritesList = [UnsplashPhoto]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(named: K.tableBackgroundColor)
+//        view.backgroundColor = UIColor(named: K.tableBackgroundColor)
         
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: K.identifierForTableCell)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.identifierForTableCell, for: indexPath) as! CustomTableViewCell
         
+        return cell
     }
 }
